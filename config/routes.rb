9a1e61cac828509c0af597f9f4c74c921c2003a2
Fredo St Fleur <fredostfleur23@gmail.com>
2023-07-19 17:35:10 -0400

    Update user ,post syntax to users, posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :user, only: [:index, :show] do
-  resources :post, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+  resources :posts, only: [:index, :show]
   end
 end
