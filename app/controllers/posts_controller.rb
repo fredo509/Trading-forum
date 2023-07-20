8d9428374072198a226053d79f7cd7b52c4e04c0
Fredo St Fleur <fredostfleur23@gmail.com>
2023-07-20 16:01:55 -0400

    update post controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   def index
-    # Logic for index action
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
-    # Logic for show action
+    @post = Post.find(params[:id])
   end
 
 end
