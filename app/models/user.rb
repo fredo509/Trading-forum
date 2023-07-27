b60f4d026b58005f698c81579ba650c82a8cb8d9
Fredo St Fleur <fredostfleur23@gmail.com>
2023-07-27 14:24:33 -0400

    Add email and password as attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+         
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
-
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  attribute :email, :string
+  attribute :encrypted_password, :string
+  validates :name, presence: true, allow_blank: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
