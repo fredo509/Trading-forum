437f4635218366090a5c54ff2f001db0ed8a4331
vvoo21 <vaneolipa@gmail.com>
2023-07-29 10:31:20 -0500

    Create authenticate_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::API
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :authenticate_request
   before_action :update_allowed_parameters, if: :devise_controller?
+
+  include jsonWebToken
 
   protected
 
   def update_allowed_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :email, :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :email, :password])
+  end
+
+  def authenticate_request
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,5 +32,4 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :text)
   end
-
 end
