c94592b971f9ac61578165730795c8656e78ffe2
vvoo21 <vaneolipa@gmail.com>
2023-07-29 14:44:30 -0500

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
- root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:new, :create]
-    end
-  end
-end
+  root "users#index"
+   resources :users, only: [:index, :show] do
+     resources :posts, only: [:index, :show, :new, :create] do
+       resources :comments, only: [:new, :create]
+       resources :likes, only: [:new, :create]
+     end
+   end
+ 
+    namespace :api, defaults: { format: :json } do
+     namespace :v1 do
+       resources :users, only: [] do
+         resources :posts, only: [:index]
+       end
+ 
+       resources :posts, only: [] do
+         resources :comments, only: [:index, :create]
+       end
+     end
+   end
+ end
